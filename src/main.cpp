030d457e2521e8946c4b402c4dfbed7d319df70a
Carlos GVL <carlos.gvl.contact@gmail.com>
2021-10-05 20:12:28 +0200

    first body

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
+void escribirEnFichero();
+
+void escribirEnFichero(){
+    ofstream archivo;
+
+    archivo.open("libros.txt",ios::out);
+
+    if(archivo.fail()){
+        cout<<"No se pudo abrir el archivo";
+        exit(1);
+    }
+    archivo<<"El Quijote de la mancha";
+
+    archivo.close();
+}
+
 int main(){
+    escribirEnFichero();
  cout<<"Hola Mundo\n";
  cout<<"Esto si funciona";
  return 0;
